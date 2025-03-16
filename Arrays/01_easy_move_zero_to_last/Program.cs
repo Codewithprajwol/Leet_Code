83093e42df1578ebadb729b7b52c2fcebc80e8d8
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-16 23:44:54 +0545

    solve: 283. Move Zeroes

---
+++ Arrays/01_easy_move_zero_to_last/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ArrayQuestion{
+
+    class Program{
+  //[0,12, 1,3,0,4]
+        public static void ZeroAtlast(int [] data){
+           int initialPostion=0;
+
+            for(int i=0;i<data.Length;i++){
+                if(data[i]!=0){
+                    data[initialPostion]=data[i];
+                    initialPostion++;
+                }
+            }
+            while(initialPostion<data.Length){
+                data[initialPostion++]=0;
+            }
+
+              Console.WriteLine($"Final answer is: [{string.Join(", ", data)}]");
+        }
+    static void Main(string [] args){
+       Program.ZeroAtlast([0,12, 1,3,0,4]);
+    }
+    }
+}
